cf87c3ed834edf686553f15171e85914a771a426
AlexKomanov <akomanov88@gmail.com>
2024-03-29 15:05:23 +0300

    Corrections with the tests

--- tests/pytest-playwright/test_playwright_manual_page.py
+++ tests/pytest-playwright/test_playwright_manual_page.py
@@ -5,8 +5,8 @@ from playwright.sync_api import Page, Playwright
 @pytest.fixture(scope='function')
 def setup(playwright: Playwright) -> Page:
     # browser = playwright.chromium.launch(headless=True, slow_mo=500, args=["--start-maximized"])
-    browser = playwright.chromium.launch(headless=True, slow_mo=500, args=["--start-maximized"])
-    page = browser.new_page(no_viewport=True)
+    browser = playwright.chromium.launch(headless=True, slow_mo=500)
+    page = browser.new_page()
 
     page.goto("https://playwright.dev/python/")
 
